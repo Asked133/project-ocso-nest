7825665159205a4dbc5dbc58d6dde13f7e972b29
Asked133 <dcgomez@outlook.com>
2025-05-17 22:18:22 -0600

    feat: add employee photo upload functionality and update employee retrieval logic

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -10,7 +10,7 @@ export class EmployeesService {
   constructor(    
   @InjectRepository(Employee)
   private employeeRepository: Repository<Employee>){}
-  
+
   async create(createEmployeeDto: CreateEmployeeDto) {
     const employee =await this.employeeRepository.save(createEmployeeDto);
     return employee;
@@ -20,19 +20,15 @@ export class EmployeesService {
     return this.employeeRepository.find();
   }
 
-  findByLocation(id: number) {
-    return this.employeeRepository.findBy({
-      location: 
-      {
-        locationId: id
-      }
-    });
-  
-  }
-  
+
   findOne(id: string) {
-    const employee = this.employeeRepository.findOneBy({
-      employeeId: id
+    const employee = this.employeeRepository.findOne({
+      where: {
+        employeeId: id
+      },
+      relations : {
+        location : true,
+      }
     });
     return employee;
 
